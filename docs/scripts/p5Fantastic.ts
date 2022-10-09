59a4d3f7e35b370ad3e95fcb662e047417989a02
ajn404 <huiyuening00@gmail.com>
2022-10-09 11:18:29 +0800

    Submit:colorful

--- docs/scripts/p5Fantastic.ts
+++ docs/scripts/p5Fantastic.ts
@@ -1474,15 +1474,15 @@ export const rayCast = (_:any) => {
 //lorenz system
 //劳伦滋混沌系统
 export const lorenzSystem = (_:any)=>{
-  let x=0.01,y=1,z=1;
+  let x=1,y=1,z=1;
   let a=10;
   let b = 28;
   let c = 8/3;
+
   _.setup = ()=>{
     _.createCanvas(800,600,_.P3D);
     _.background(0);
-    _.fill(237, 34, 93);
-    _.strokeWeight(0.1);
+    _.strokeWeight(0.2);
   }
   _.draw = ()=>{
     if (window && window["p5DrawLoop"] !== "lorenzSystem") {
@@ -1496,7 +1496,7 @@ export const lorenzSystem = (_:any)=>{
     y+=dy;
     z+=dz;
     _.translate(_.width/2,_.height/2, 0);
-    _.stroke(237, 34, 93);
+    _.stroke((x<0?-x:x)*10%255,(y<0?-y:y)*10%255,(z<0?-z:z)*10%255);
     _.scale(10);
     _.point(x,y,z);
   }
