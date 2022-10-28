0d3fa1ca6fee348931206f983f945da51dec734a
ajn404 <huiyuening00@gmail.com>
2022-10-28 17:49:38 +0800

    Submit:只能看到脚

--- docs/components/fantastic/threeLoadModel.vue
+++ docs/components/fantastic/threeLoadModel.vue
@@ -1,139 +1,125 @@
 <template>
     <div class="fullscreen" ref="container">
+        <canvas ref="canvasItem"></canvas>
     </div>
 </template>
 
 <script lang="ts" setup>
+//@ts-ignore
 import { ref, nextTick, onUnmounted } from "vue";
 import * as THREE from "three";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
-interface optionsConfig {
-    container: HTMLElement
-}
-
-class Sketch {
-    time: number;
-    geometry: any;
-    material: any;
-    mesh: any;
-    scene: any;
-    renderer: any;
-    camera: any;
-    container: any;
-    width: any;
-    height: any;
-    control: any;
-
-    init(options: optionsConfig) {
-        this.time = 10;
-        this.container = options.container;
-        this.width = this.container.clientWidth || window.innerWidth;
-        this.height = this.container.clientHeight || window.innerHeight;
-
-        this.scene = new THREE.Scene();
-        this.scene.background = new THREE.Color('white');
-
-        const fov = 45;
-        const aspect = 2;  // the canvas default
-        const near = 0.1;
-        const far = 100;
-        this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        this.camera.position.set(0, 10, 20);
-
-        this.renderer = new THREE.WebGLRenderer({
-            antialias: true,
-            alpha: true,
-        });
-        this.renderer.setSize(this.width, this.height);
-        this.container.appendChild(this.renderer.domElement);
-
-        this.control = new OrbitControls(this.camera, this.renderer.domElement);
-
-        this.resize()
-        addEventListener("resize", this.resize.bind(this), false)
-        this.addObject();
-        this.animate();
-    }
-    animate() {
-        this.render();
-        if (this.container.getBoundingClientRect().width > 0)
-            requestAnimationFrame(this.animate.bind(this));
-        else return;
-    }
-
-    render() {
-        this.renderer.render(this.scene, this.camera);
-    }
-
-    resize() {
-        this.camera.aspect = this.container.clientWidth / this.container.clientHeight;
-        this.camera.updateProjectionMatrix();
-        this.renderer.setSize(this.container.clientWidth, this.container.clientHeight);
-        this.render();
-    }
-
-    addObject() {
-        const objLoader = new OBJLoader();
-
-        const mtlLoader = new MTLLoader();
-        mtlLoader.load('/note/model/diaona.mtl', (mtl) => {
-            mtl.preload();
-            objLoader.setMaterials(mtl);
-            objLoader.load('/note/model/diaona.obj', (root) => {
-                this.scene.clear()
-                this.scene.add(root);
-            });
-        })
-
-    }
-
-    beforeDestroy() {
-
-        const disposeChild = (mesh: any) => {
-            if (mesh instanceof THREE.Mesh) {
-                if (mesh.geometry?.dispose) {
-                    mesh.geometry.dispose();
-                }
-                if (mesh.material?.dispose) {
-                    mesh.material.dispose();
-                }
-                if (mesh.material?.texture?.dispose) {
-                    mesh.material.texture.dispose();
-                }
-            }
-            if (mesh instanceof THREE.Group) {
-                mesh.clear();
-            }
-            if (mesh instanceof THREE.Object3D) {
-                mesh.clear();
-            }
-        }
-        this.scene.traverse((item: any) => {
-            disposeChild(item);
-        })
-        THREE.Cache.clear();
-        this.scene.clear();
-        this.renderer.dispose();
-        this.renderer.forceContextLoss();
-    }
-}
-
 
 const container = ref(null);
-let sketch = new Sketch();
+const canvasItem = ref(null);
+const options = {
+        container: container.value || document.body
+    };
+
+function main() {
+  const canvas =  canvasItem.value;
+  const renderer = new THREE.WebGLRenderer({canvas});
+  renderer.outputEncoding = THREE.sRGBEncoding;
+
+  const fov = 45;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 100;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.set(0, 10, 20);
+
+  const controls = new OrbitControls(camera, canvas);
+  controls.target.set(0, 5, 0);
+  controls.update();
+
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color('black');
+
+  {
+    const planeSize = 40;
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('https://threejs.org/manual/examples/resources/images/checker.png');
+    texture.encoding = THREE.sRGBEncoding;
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.rotation.x = Math.PI * -.5;
+    scene.add(mesh);
+  }
+
+  {
+    const skyColor = 0xB1E1FF;  // light blue
+    const groundColor = 0xB97A20;  // brownish orange
+    const intensity = 1;
+    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+    scene.add(light);
+  }
+
+  {
+    const color = 0xFFFFFF;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(5, 10, 2);
+    scene.add(light);
+    scene.add(light.target);
+  }
+
+  {
+    const mtlLoader = new MTLLoader();
+    mtlLoader.load('/note/model/diaona.mtl', (mtl) => {
+      mtl.preload();
+      const objLoader = new OBJLoader();
+      objLoader.setMaterials(mtl);
+      objLoader.load('/note/model/diaona.obj', (root) => {
+        scene.add(root);
+      });
+    });
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = container?.value?.clientWidth|| canvas.clientWidth;
+    const height = container?.value?.clientHeight||canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function render() {
+
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+
+    requestAnimationFrame(render);
+  }
+
+  requestAnimationFrame(render);
+}
+
+
 
 
 
 nextTick(() => {
-    const options = {
-        container: container.value || document.body
-    };
-    sketch.init(options);
-})
-
-onUnmounted(() => {
-    sketch.beforeDestroy();
+    main();
 })
 </script>
