23248178e68ad0ff4e7be6f5509645d01ddf2360
ajn404 <huiyuening00@gmail.com>
2022-11-02 14:52:26 +0800

    Submit:

--- docs/codeReference/leetcode/14.最长公共前缀.js
+++ docs/codeReference/leetcode/14.最长公共前缀.js
@@ -10,7 +10,33 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+    let min =strs[0].length ;
+    let res=true;
+    strs.forEach((item,index)=>{
+        if(item[0]!==strs[0][0]){
+            res =  false
+        }
+        let temp = twoSame(item,strs[0])||twoSame(strs[0],item)
+        if(temp<min){
+            min = temp
+        }
 
+
+    })
+
+    return res?strs[0].slice(0,min):""
 };
+
+const twoSame=(s1,s2)=>{
+    let temp = ""
+    for(let i=0;i<s1.split("").length;i++){
+        let item = s1.split("")[i];
+        temp+=  item;
+        if(s2.indexOf(temp)===-1){
+            return i
+        }  
+    }
+}
+
 // @lc code=end
 
--- docs/markdown/notes/leetcode.md
+++ docs/markdown/notes/leetcode.md
@@ -6,3 +6,11 @@
 @[code](../../codeReference/leetcode/9.回文数.js)
 ### 罗马数字转整数
 @[code](../../codeReference/leetcode/13.罗马数字转整数.js)
+### 最长公共前缀
+@[code](../../codeReference/leetcode/14.最长公共前缀.js)
+:::tip
+Accepted
+124/124 cases passed (108 ms)
+Your runtime beats 7.49 % of javascript submissions
+Your memory usage beats 5.01 % of javascript submissions (47.2 MB)
+:::
