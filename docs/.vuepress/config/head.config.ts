d7bc18f90df32d39cbe17fb6b9cb1c55d2359ad9
ajn404 <huiyuening00@gmail.com>
2022-11-02 18:21:49 +0800

    Submit:

--- docs/.vuepress/config/head.config.ts
+++ docs/.vuepress/config/head.config.ts
@@ -3,8 +3,9 @@ import { HeadConfig } from "vuepress"
 
 const head: HeadConfig []= [
     ['link', { rel: 'icon', href: '/note/images/logo.jpg' }],
-    ['script',{src:'/note/js/p5.min.js'}],
-    ['script',{src:'/note/js/p5.sound.min.js'}]
+    ['script',{src:'https://p5js.org/assets/js/p5.min.js?v=9299cb'}],
+    ['script',{src:'https://p5js.org/assets/js/p5.sound.min.js?v=f067ef'}]
+
 ]
 //https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/p5.min.js
 //cdn的缺点是，没有网，很难受
