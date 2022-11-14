00beb2b729d67064e28dcb48f6434d2c1f1c032c
ajn404 <huiyuening00@gmail.com>
2022-11-14 18:04:09 +0800

    Submit:

--- docs/components/tools/toolPlayUseJs.vue
+++ docs/components/tools/toolPlayUseJs.vue
@@ -5,14 +5,14 @@ const link: Ref<HTMLAudioElement | null> = ref(null)
 const hover: Ref<HTMLAudioElement | null> = ref(null)
 
 const linkMusicOn = ref(false)
-const musicLinkFunc = function(){
+const musicLinkFunc = function () {
     if (link.value) link.value.play()
 }
 
 const music = () => {
     if (!linkMusicOn.value) {
         [].forEach.call(document.querySelectorAll("a"), (a: HTMLElement) => {
-            a.addEventListener('click',musicLinkFunc)
+            a.addEventListener('click', musicLinkFunc)
         });
         linkMusicOn.value = true;
     } else {
@@ -23,19 +23,18 @@ const music = () => {
     }
 }
 linkObj.value = {
-        title:'此功能用于给当前页面的所有链接添加点击声音',
-        click: music
-    }
-
-
-
+    title: '此功能用于给当前页面的所有链接添加点击声音',
+    click: music
+}
 </script>
-
 <template>
     <div class="container">
         <el-button @click="linkObj.click" :title="linkObj.title">
-            {{ linkMusicOn?'turn off link music':'turn on link music' }}
+            {{ linkMusicOn ? 'turn off link music' : 'turn on link music' }}
         </el-button>
+
+        
+        <!-- do not show -->
         <audio id="link" src="/note/music/link.mp3" preload="auto" ref="link"></audio>
         <audio id="hover-audio" src="/note/music/hover.mp3" preload="auto" ref="hover"></audio>
     </div>
