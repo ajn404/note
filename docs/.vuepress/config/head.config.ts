3625ff7d4e83ad157db3ca2a870f25374d2e8f78
ajn404 <huiyuening00@gmail.com>
2022-11-22 14:44:53 +0800

    Submit:

--- docs/.vuepress/config/head.config.ts
+++ docs/.vuepress/config/head.config.ts
@@ -4,8 +4,10 @@ import { HeadConfig } from "vuepress"
 const head: HeadConfig []= [
     ['link', { rel: 'icon', href: '/note/images/logo.jpg' }],
     ['link', { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/@animxyz/core' }],
+    ['link',{rel: 'stylesheet', href:'/note/data/asciinema-player.css'}],
     // ['script',{src:'https://p5js.org/assets/js/p5.min.js?v=9299cb',defer:true}],
     // ['script',{src:'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/p5.min.js',defer:true}],
     ['script',{src:'/note/p5.min.js'}],
+    ['script',{src:'/note/data/asciinema-player.min.js'}],
 ]
 export default head;
---
+++ docs/components/UI/asciinemaPlayerBox.vue
@@ -0,0 +1,37 @@
+
+
+<script lang="ts" setup>
+// import * as AsciinemaPlayer from 'asciinema-player';
+// import 'asciinema-player/dist/bundle/asciinema-player.css'
+import { nextTick, ref } from 'vue'
+const player = ref(null)
+const themeList = ["asciinema","monokai","tango","solarized-dark","solarized-light"]
+const theme = themeList[Math.floor(Math.random()*10%themeList.length)]
+const AsciinemaPlayer = window['AsciinemaPlayer'];
+
+nextTick(() => {
+    AsciinemaPlayer.create('/note/data/ncu.cast', player.value,
+        {
+            loop: true,
+            theme: theme
+        });
+})
+</script>
+
+<template>
+    <div class="player-box">
+        <div ref="player" class="player"></div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.player{
+    :deep(.asciinema-terminal){
+    max-width: calc(100% - 3rem - 1.5em);
+    span{
+        max-width: 100%;
+    }
+}
+}
+
+</style>
